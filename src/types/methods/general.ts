e8768ba987db70c2f271e9a7a8aa830649568185
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-07-29 04:29:14 +0200

    feat: Basic SDK

---
+++ src/types/methods/general.ts
@@ -0,0 +1,36 @@
+import { IAdenaResponse } from "../common";
+
+export enum AddEstablishResponseType {
+	CONNECTION_SUCCESS = "CONNECTION_SUCCESS",
+	ALREADY_CONNECTED = "ALREADY_CONNECTED",
+}
+
+export type AddEstablishResponse = IAdenaResponse<AddEstablishResponseType, {}>;
+
+export type AdenaAddEstablish = (name: string) => Promise<AddEstablishResponse>;
+
+enum GetAccountResponseType {
+	GET_ACCOUNT = "GET_ACCOUNT",
+}
+
+enum EAccountStatus {
+	ACTIVE = "ACTIVE",
+	INACTIVE = "IN_ACTIVE",
+}
+
+export type GetAccountResponseData = {
+	accountNumber: string;
+	address: string;
+	coins: string;
+	chainId: string;
+	sequence: string;
+	status: EAccountStatus;
+	public_key: {
+		"@type": string;
+		value: string;
+	};
+};
+
+type GetAccountResponse = IAdenaResponse<GetAccountResponseType, GetAccountResponseData>;
+
+export type AdenaGetAccount = () => Promise<GetAccountResponse>;
